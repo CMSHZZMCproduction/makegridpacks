e67f39a670ead218d729bbdce1c4b5aff776f983
Heshy Roskes <jroskes1@jhu.edu>
2017-11-07 16:06:54 +0100

    first commit

---
+++ makegridpacks.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+
+import csv, glob, os, random, re, shutil, subprocess, sys, urllib
+
+from utilities import cd, KeepWhileOpenFile, LSB_JOBID, mkdir_p, mkdtemp, TFile
+
+#do not change these once you've started making tarballs!
+#they are included in the tarball name and the script
+#will think the tarballs don't exist even if they do
+cmsswversion = "CMSSW_9_3_0"
+scramarch = "slc6_amd64_gcc630"
+
+genproductions = os.path.join(os.environ["CMSSW_BASE"], "src", "genproductions")
+if not os.path.exists(genproductions) or os.environ["CMSSW_VERSION"] != cmsswversion or os.environ["SCRAM_ARCH"] != scramarch:
+  raise RuntimeError("Need to cmsenv in a " + cmsswversion + " " + scramarch + " release that contains genproductions")
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+class MCSample(object):
+  def __init__(self, productionmode, mass):
+    self.productionmode = productionmode
+    self.mass = int(str(mass))
+
+  def __eq__(self, other):
+    return self.productionmode == other.productionmode and self.mass == other.mass
+  def __ne__(self, other):
+    return not (self == other)
+  def __hash__(self):
+    return hash((self.productionmode, self.mass))
+  def __str__(self):
+    return "{} {}".format(self.productionmode, self.mass)
+  def __repr__(self):
+    return "{}({!r}, {!r})".format(type(self).__name__, self.productionmode, self.mass)
+
+  @property
+  def powhegprocess(self):
+    if self.productionmode == "ggH": return "gg_H_quark-mass-effects"
+    if self.productionmode == "VBF": return "VBF_H"
+    if self.productionmode == "ZH": return "HZJ"
+    if self.productionmode in ("WplusH", "WminusH"): return "HWJ"
+    if self.productionmode == "ttH": return "ttH"
+    raise ValueError("Unknown productionmode "+self.productionmode)
+
+  @staticmethod
+  @cache
+  def makecards(folder):
+    with cd(folder):
+      subprocess.check_call(["./makecards.py"])
+
+  @property
+  def powhegcard(self):
+    folder = os.path.join(genproductions, "bin", "Powheg", "production", "2017", "13TeV", self.powhegprocess+"_NNPDF31_13TeV")
+    makecards(folder)
+
+    cardbase = self.powhegprocess
+    if self.productionmode == "ZH": cardbase = "HZJ_HanythingJ"
+    if self.productionmode == "WplusH": cardbase = "HWplusJ_HanythingJ"
+    if self.productionmode == "WminusH": cardbase = "HWminusJ_HanythingJ"
+    if self.productionmode == "ttH": cardbase = "ttH_inclusive"
+    card = os.path.join(folder, cardbase+"_NNPDF31_13TeV_M{:d}.input".format(self.mass))
+
+    if not os.path.exists(card):
+      raise IOError(card+" does not exist")
+    return card
+
+  @property
+  def filter4L(self):
+    if self.productionmode in ("ggH", "VBF", "WplusH", "WminusH"): return False
+    if self.productionmode in ("ZH", "ttH"): return True
+    raise ValueError("Unknown productionmode "+self.productionmode)
+
+  @property
+  def reweightdecay(self):
+    return self.mass >= 200
+
+  @property
+  def JHUGencard(self):
+    folder = os.path.join(genproductions, "bin", "JHUGen", "cards", "decay")
+
+    filename = "ZZ4l" if self.filter4L else "ZZ2l2any"
+    filename += "_withtaus"
+    if self.reweightdecay: filename += "_reweightdecay_CPS"
+    filename += ".input"
+
+    card = os.path.join(folder, filename)
+    
+    if not os.path.exists(card):
+      raise IOError(card+" does not exist")
+    return card
+
+  @property
+  def tarballversion(self):
+    return 1
+
+  @property
+  def cvmfstarball(self):
+    folder = os.path.join("/cvmfs/cms.cern.ch/phys_generator/gridpacks/2017/13TeV/powheg/V2", self.powhegprocess+"_NNPDF31_13TeV")
+    tarballname = os.path.basename(self.powhegcard).replace(".input", ".tgz")
+    return os.path.join(folder, "v{}".format(self.tarballversion, tarballname)
+
+  @property
+  def eostarball(self):
+    return self.cvmfstarball.replace("/cvmfs/cms.cern.ch/phys_generator/", "/eos/cms/store/group/phys_generator/cvmfs/")
+
+  @property
+  def foreostarball(self):
+    """to put in a directory structure here, which will later be copied to eos"""
+    return self.cvmfstarball.replace("/cvmfs/cms.cern.ch/phys_generator/", here+"/")
+
+  @property
+  def workdir(self):
+    return os.path.join()
+
+  @property
+  def tmptarball(self):
+    return os.path.join(here, "workdir", os.path.basename(self.powhegcard).replace(".input", ""),
+             self.powhegprocess+"_"+scramarch+"_"+cmsswversion+"_"+os.path.basename(self.powhegcard).replace(".input", ".tgz"))
+
+  @property
+  def makegridpackcommand(self):
+    args = {
+      "-p": "f",
+      "-i": self.powhegcard,
+      "-g": self.JHUGencard,
+      "-m": self.powhegprocess,
+      "-f": os.path.basename(self.powhegcard).replace(".input", ""),
+      "-q": "",
+      "-n": "10",
+    }
+
+    return [os.path.join(genproductions, "bin", "Powheg", "run_pwg.py")] + sum(([k, v] for k, v in args.iteritems()), [])
+
+  def makegridpack(self):
+    if os.path.exists(self.cvmfstarball): return "exists on cvmfs"
+    if os.path.exists(self.eostarball): return "exists on eos, not yet copied to cvmfs"
+    if os.path.exists(self.foreostarball): return "exists in this folder, to be copied to eos"
+
+    workdir = os.path.dirname(self.tmptarball)
+    mkdir_p(workdir)
+    with cd(workdir), KeepWhileOpenFile(self.tmptarball+".tmp") as kwof:
+      if not kwof: return "job to make the tarball is already running"
+      if not LSB_JOBID(): return "would run: "+" ".join(self.makegridpackcommand)
+
+      subprocess.check_call(self.makegridpackcommand)
+      mkdir_p(os.path.dirname(self.foreostarball))
+      shutil.move(self.tmptarball, self.foreostarball)
+      os.rmtree(os.path.dirname(self.tmptarball))
+      return "tarball is created and moved to this folder, to be copied to eos"
+
+def getmasses(productionmode):
+  if productionmode in "ggH", "VBF", "WplusH", "WminusH", "ZH":
+    return 115, 120, 124, 125, 126, 130, 135, 140, 145, 150, 155, 160, 165, 170, 175, 180, 190, 200, 210, 230, 250, 270, 300, 350, 400, 450, 500, 550, 600, 700, 750, 800, 900, 1000, 1500, 2000, 2500, 3000
+  if productionmode == "ttH":
+    return 115, 120, 124, 125, 126, 130, 135, 140, 145
+
+def makegridpacks():
+  for productionmode in "ggH", "VBF", "WplusH", "WminusH", "ZH", "ttH":
+    for mass in getmasses(productionmode):
+      MCSample(productionmode, mass).makegridpack()
---
+++ utilities.py
@@ -0,0 +1,111 @@
+import contextlib, errno, functools, itertools, logging, os, tempfile
+
+def mkdir_p(path):
+  """http://stackoverflow.com/a/600612/5228524"""
+  try:
+    os.makedirs(path)
+  except OSError as exc:
+    if exc.errno == errno.EEXIST and os.path.isdir(path):
+      pass
+    else:
+      raise
+
+@contextlib.contextmanager
+def cd(newdir):
+  """http://stackoverflow.com/a/24176022/5228524"""
+  prevdir = os.getcwd()
+  os.chdir(os.path.expanduser(newdir))
+  try:
+    yield
+  finally:
+    os.chdir(prevdir)
+
+class TFile(object):
+  def __init__(self, *args, **kwargs):
+    self.args, self.kwargs = args, kwargs
+  def __enter__(self):
+    import ROOT
+    self.__tfile = ROOT.TFile.Open(*self.args, **self.kwargs)
+    return self.__tfile
+  def __exit__(self, *err):
+    self.__tfile.Close()
+
+def mkdtemp(**kwargs):
+  if "dir" not in kwargs:
+    if LSB_JOBID() is not None:
+      kwargs["dir"] = "/pool/lsf/hroskes/{}/".format(LSB_JOBID())
+  return tempfile.mkdtemp(**kwargs)
+
+class KeepWhileOpenFile(object):
+  def __init__(self, name, message=None):
+    logging.debug("creating KeepWhileOpenFile {}".format(name))
+    self.filename = name
+    self.__message = message
+    self.pwd = os.getcwd()
+    self.fd = self.f = None
+    self.bool = False
+
+  def __enter__(self):
+    logging.debug("entering KeepWhileOpenFile {}".format(self.filename))
+    with cd(self.pwd):
+      logging.debug("does it exist? {}".format(os.path.exists(self.filename)))
+      try:
+        logging.debug("trying to open")
+        self.fd = os.open(self.filename, os.O_CREAT | os.O_EXCL | os.O_WRONLY)
+      except OSError:
+        logging.debug("failed: it already exists")
+        return None
+      else:
+        logging.debug("succeeded: it didn't exist")
+        logging.debug("does it now? {}".format(os.path.exists(self.filename)))
+        if not os.path.exists(self.filename):
+          logging.warning("{} doesn't exist!??".format(self.filename))
+        self.f = os.fdopen(self.fd, 'w')
+        try:
+          if self.__message is not None:
+            logging.debug("writing message")
+            self.f.write(self.__message+"\n")
+            logging.debug("wrote message")
+        except IOError:
+          logging.debug("failed to write message")
+          pass
+        try:
+          logging.debug("trying to close")
+          self.f.close()
+          logging.debug("closed")
+        except IOError:
+          logging.debug("failed to close")
+          pass
+        self.bool = True
+        return True
+
+  def __exit__(self, *args):
+    logging.debug("exiting")
+    if self:
+      try:
+        with cd(self.pwd):
+          logging.debug("trying to remove")
+          os.remove(self.filename)
+          logging.debug("removed")
+      except OSError:
+        logging.debug("failed")
+        pass #ignore it
+    self.fd = self.f = None
+    self.bool = False
+
+  def __nonzero__(self):
+    return self.bool
+
+def cache(function):
+  cache = {}
+  @functools.wraps(function)
+  def newfunction(*args, **kwargs):
+    try:
+      return cache[args, tuple(sorted(kwargs.iteritems()))]
+    except TypeError:
+      print args, tuple(sorted(kwargs.iteritems()))
+      raise
+    except KeyError:
+      cache[args, tuple(sorted(kwargs.iteritems()))] = function(*args, **kwargs)
+      return newfunction(*args, **kwargs)
+  return newfunction
