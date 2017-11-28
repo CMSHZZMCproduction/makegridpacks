2d1f0ac2392c1d4bc0ff8539183ba768e89a68c5
Heshy Roskes <jroskes1@jhu.edu>
2017-11-28 22:11:56 +0100

    update times from McM

--- fixrequests.py
+++ fixrequests.py
@@ -6,7 +6,8 @@ from makegridpacks import *
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
-  parser.add_argument("--dry-run", "-n", action="store_true")
+  parser.add_argument("--dry-run", "-n", action="store_true", help="don't send anything to McM")
+  parser.add_argument("--times", "-t", action="store_true", help="get the times from McM for all requests")
   args = parser.parse_args()
 
   with RequestQueue() as queue:
@@ -14,9 +15,9 @@ if __name__ == "__main__":
       for decaymode in "4l", "2l2nu", "2l2q":
         for mass in getmasses(productionmode, decaymode):
           sample = MCSample(productionmode, decaymode, mass)
-          if sample.needsupdate and sample.prepid and os.path.exists(sample.cvmfstarball):
+          if (sample.needsupdate or args.times_only) and sample.prepid and os.path.exists(sample.cvmfstarball):
             sample.gettimepereventfromMcM()
             print sample
-            if not args.dry_run:
+            if sample.needsupdate and not args.dry_run:
               queue.addrequest(sample, useprepid=True)
 
