a458b7576e4f29d2203391ceb88e33c6249b8912
Heshy Roskes <jroskes1@jhu.edu>
2017-11-29 14:21:22 +0100

    fix fixrequests

--- fixrequests.py
+++ fixrequests.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
       for decaymode in "4l", "2l2nu", "2l2q":
         for mass in getmasses(productionmode, decaymode):
           sample = MCSample(productionmode, decaymode, mass)
-          if (sample.needsupdate or args.times_only) and sample.prepid and os.path.exists(sample.cvmfstarball):
+          if (sample.needsupdate or args.times) and sample.prepid and os.path.exists(sample.cvmfstarball):
             sample.gettimepereventfromMcM()
             print sample
             if sample.needsupdate and not args.dry_run:
