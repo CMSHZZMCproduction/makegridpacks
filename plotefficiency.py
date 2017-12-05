1e14d579d251c027762001f563433a0f22c389c3
Heshy Roskes <jroskes1@jhu.edu>
2017-12-05 22:51:31 +0100

    chi2, style

--- plotefficiency.py
+++ plotefficiency.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python
 
-import array, ROOT
+import array, ROOT, style
 
 from makegridpacks import *
 
 c = ROOT.TCanvas()
 
 for p in "ZH", "ttH":
+  print p
+  print
   x = array.array('d', getmasses(p, "4l"))
   y = array.array('d', [MCSample(p, "4l", m).matchefficiency for m in getmasses(p, "4l")])
   ex = array.array('d', [0 for m in x])
   ey = array.array('d', [MCSample(p, "4l", m).matchefficiencyerror for m in getmasses(p, "4l")])
   g = ROOT.TGraphErrors(len(x), x, y, ex, ey)
   g.Draw("AP")
+  f = ROOT.TF1("constant", "[0]", min(x), max(x))
+  g.Fit(f)
+  print
+  print "chi2/NDF =", f.GetChisquare()/f.GetNDF()
+  print "prob =", f.GetProb()
+  print
+  print
+  print
   for ext in "png", "pdf":
     c.SaveAs("~/www/TEST/"+p+"."+ext)
