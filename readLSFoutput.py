2970c80520db47d221476588d4b3b5af552eef79
Heshy Roskes <jroskes1@jhu.edu>
2017-12-07 14:27:34 +0100

    read LSF output

---
+++ readLSFoutput.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from makegridpacks import *
+
+for stdout in glob.glob("LSFJOB_*/STDOUT"):
+  with open(stdout) as f:
+    for line in f:
+      match = re.match("(.*) size and time per event are found to be (.*) and (.*), will send it to McM", line.strip())
+      if match:
+        sample = MCSample(*match.group(1).split())
+        sample.sizeperevent = float(match.group(2))
+        sample.timeperevent = float(match.group(3))
