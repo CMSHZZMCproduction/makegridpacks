436547ae374fc2ffeae3ebcdade3cf6fc1fa87e0
Heshy Roskes <jroskes1@jhu.edu>
2017-12-07 21:08:59 +0100

    move some functions around

--- makegridpacks.py
+++ makegridpacks.py
@@ -51,7 +51,7 @@ class MCSampleBase(JsonDict):
   @abc.abstractproperty
   def fragmentname(self): pass
   @abc.abstractproperty
-  def fragmenttag(self): pass
+  def genproductionscommit(self): pass
 
   def __eq__(self, other):
     return self.keys == other.keys
@@ -399,7 +399,7 @@ class MCSampleBase(JsonDict):
       "gridpack location": self.cvmfstarball,
       "cards url": self.cardsurl,
       "fragment name": self.fragmentname,
-      "fragment tag": self.fragmenttag,
+      "fragment tag": self.genproductionscommit,
       "mcm tag": self.tags,
       "mcdbid": 0,
       "time per event [s]": self.timeperevent if self.timeperevent is not None else self.defaulttimeperevent,
@@ -462,78 +462,10 @@ class MassScanMCSample(MCSampleBase):
   @property
   def identifiers(self):
     return self.productionmode, self.decaymode, self.mass
-
-class POWHEGJHUGenMCSample(MCSampleBase):
   @abc.abstractproperty
-  def powhegprocess(self): pass
-  @abc.abstractproperty
-  def powhegcard(self): pass
+  def reweightdecay(self): False
   @property
-  def hasfilter(self): return "filter" in self.JHUGencard.lower()
-  @property
-  def tmptarball(self):
-    return os.path.join(here, "workdir", os.path.basename(self.powhegcard).replace(".input", "_"+self.decaymode),
-             self.powhegprocess+"_"+scramarch+"_"+cmsswversion+"_"+os.path.basename(self.powhegcard).replace(".input", "_"+self.decaymode+".tgz"))
-  @property
-  def queue(self):
-    if self.productionmode == "ggH": return "1nd"
-    if self.productionmode in ("ZH", "ttH"): return "1nw"
-    return "1nd"
-  @property
-  def makegridpackcommand(self):
-    args = {
-      "-p": "f",
-      "-i": self.powhegcard,
-      "-g": self.JHUGencard,
-      "-m": self.powhegprocess,
-      "-f": os.path.basename(self.powhegcard).replace(".input", "_"+self.decaymode),
-      "-q": self.queue,
-      "-n": "10",
-    }
-    return ["./run_pwg.py"] + sum(([k, v] for k, v in args.iteritems()), [])
-  @property
-  def makinggridpacksubmitsjob(self):
-    return "full_"+os.path.basename(self.powhegcard).replace(".input", "")
-
-class POWHEGJHUGenMassScanMCSample(MassScanMCSample, POWHEGJHUGenMCSample):
-  @property
-  def powhegprocess(self):
-    if self.productionmode == "ggH": return "gg_H_quark-mass-effects"
-    if self.productionmode == "VBF": return "VBF_H"
-    if self.productionmode == "ZH": return "HZJ"
-    if self.productionmode in ("WplusH", "WminusH"): return "HWJ"
-    if self.productionmode == "ttH": return "ttH"
-    raise ValueError("Unknown productionmode "+self.productionmode)
-
-  @property
-  def powhegcard(self):
-    folder = os.path.join(genproductions, "bin", "Powheg", "production", "2017", "13TeV", self.powhegprocess+"_NNPDF31_13TeV")
-    makecards(folder)
-
-    cardbase = self.powhegprocess
-    if self.productionmode == "ZH": cardbase = "HZJ_HanythingJ"
-    if self.productionmode == "WplusH": cardbase = "HWplusJ_HanythingJ"
-    if self.productionmode == "WminusH": cardbase = "HWminusJ_HanythingJ"
-    if self.productionmode == "ttH": cardbase = "ttH_inclusive"
-    card = os.path.join(folder, cardbase+"_NNPDF31_13TeV_M{:d}.input".format(self.mass))
-
-    if not os.path.exists(card):
-      raise IOError(card+" does not exist")
-    return card
-
-  @property
-  def reweightdecay(self):
-    return self.mass >= 200
-
-  @property
-  def filter4L(self):
-    if self.decaymode != "4l": return False
-    if self.productionmode in ("ggH", "VBF", "WplusH", "WminusH"): return False
-    if self.productionmode in ("ZH", "ttH"): return True
-    raise ValueError("Unknown productionmode "+self.productionmode)
-
-  @property
-  def JHUGencard(self):
+  def decaycard(self):
     folder = os.path.join(genproductions, "bin", "JHUGen", "cards", "decay")
 
     if self.decaymode == "4l":
@@ -559,75 +491,6 @@ class POWHEGJHUGenMassScanMCSample(MassScanMCSample, POWHEGJHUGenMCSample):
     if not os.path.exists(card):
       raise IOError(card+" does not exist")
     return card
-
-  @property
-  def tarballversion(self):
-    v = 1
-
-    v+=1 #JHUGen version
-    if self.productionmode == "ggH" and self.decaymode == "2l2nu" and self.mass == 400: v+=1
-    if self.productionmode == "ggH" and self.decaymode == "4l" and self.mass in (300, 350, 400, 450, 500, 550, 600, 700, 750, 800, 900, 1000, 1500, 2000, 2500, 3000): v+=1  #core dumps in v2
-    if self.productionmode == "ggH" and self.decaymode == "2l2nu" and self.mass in (300, 400, 1000, 1500): v+=1   #core dumps in v1
-    if self.productionmode == "ggH" and self.decaymode == "2l2q" and self.mass == 750: v+=1   #core dumps in v1
-    if self.productionmode == "ZH" and self.decaymode == "4l" and self.mass == 145: v+=1   #core dumps in v2
-    if self.decaymode == "4l": v+=1  #v1 messed up the JHUGen decay card
-    if self.productionmode == "ggH" and self.decaymode == "2l2nu" and self.mass == 2500: v+=1  #v1 is corrupted
-    if self.productionmode == "ggH" and self.decaymode == "2l2q" and self.mass == 800: v+=1  #same
-
-    return v
-
-  @property
-  def cvmfstarball(self):
-    folder = os.path.join("/cvmfs/cms.cern.ch/phys_generator/gridpacks/2017/13TeV/powheg/V2", self.powhegprocess+"_NNPDF31_13TeV")
-    tarballname = os.path.basename(self.powhegcard).replace(".input", ".tgz")
-    if self.decaymode != "4l":
-      decaymode = self.decaymode
-      if "ZZ2l2any_withtaus.input" in self.JHUGencard: decaymode == "2l2X"
-      elif "ZZany_filter2lOSSF.input" in self.JHUGencard: decaymode = "_filter2l"
-      tarballname = tarballname.replace("NNPDF31", "ZZ"+self.decaymode+"_NNPDF31")
-    return os.path.join(folder, tarballname.replace(".tgz", ""), "v{}".format(self.tarballversion), tarballname)
-
-  @property
-  @cache
-  def olddatasetname(self):
-    p = self.productionmode
-    if p == "VBF": p = "VBFH"
-    with contextlib.closing(urllib.urlopen("https://raw.githubusercontent.com/CJLST/ZZAnalysis/miniAOD_80X/AnalysisStep/test/prod/samples_2016_MC.csv")) as f:
-      reader = csv.DictReader(f)
-      for row in reader:
-        if row["identifier"] == "{}{}".format(p, self.mass):
-          dataset = row["dataset"]
-          result = re.sub(r"^/([^/]*)/[^/]*/[^/]*$", r"\1", dataset)
-          assert result != dataset and "/" not in result, result
-          if self.decaymode == "4l":
-            return result
-    raise ValueError("Nothing for {}".format(self))
-
-  @property
-  def datasetname(self):
-    if self.decaymode == "2l2nu":
-      result = type(self)(self.productionmode, "4l", self.mass).datasetname.replace("4L", "2L2Nu")
-    elif self.decaymode == "2l2q":
-      result = type(self)(self.productionmode, "4l", self.mass).datasetname.replace("4L", "2L2Q")
-      if self.mass == 125:
-        if self.productionmode in ("VBF", "WplusH", "WminusH"): result = result.replace("2L2Q", "2L2X")
-        if self.productionmode == "ZH": result = "ZH_HToZZ_2LFilter_M125_13TeV_powheg2-minlo-HZJ_JHUGenV709_pythia8"
-        if self.productionmode == "ttH": result = "ttH_HToZZ_2LOSSFFilter_M125_13TeV_powheg2_JHUGenV709_pythia8"
-    elif self.productionmode in ("WplusH", "WminusH", "ZH") and self.mass > 230:
-      result = type(self)(self.productionmode, self.decaymode, 230).datasetname.replace("M230", "M{:d}".format(self.mass))
-    else:
-      result = self.olddatasetname.replace("JHUgenV698", "JHUGenV709").replace("JHUgenV6", "JHUGenV709")
-
-    pm = self.productionmode.replace("gg", "GluGlu")
-    dm = self.decaymode.upper().replace("NU", "Nu")
-    if self.decaymode == "2l2q" and self.mass == 125:
-      if self.productionmode in ("VBF", "WplusH", "WminusH"): dm = "2L2X"
-      if self.productionmode in ("ZH", "ttH"): dm = "Filter"
-    searchfor = [pm, dm, "M{:d}".format(self.mass), "JHUGenV709_"]
-    if any(_ not in result for _ in searchfor):
-      raise ValueError("Dataset name doesn't make sense:\n{}\n{}\n{}".format(result, searchfor, self))
-
-    return result
 
   @property
   def nevents(self):
@@ -668,18 +531,46 @@ class POWHEGJHUGenMassScanMCSample(MassScanMCSample, POWHEGJHUGenMCSample):
 
     raise ValueError("No nevents for {}".format(self))
 
+class POWHEGJHUGenMCSample(MCSampleBase):
+  @abc.abstractproperty
+  def powhegprocess(self): pass
+  @abc.abstractproperty
+  def powhegcard(self): pass
   @property
-  def generators(self):
-    return r"powheg\ {} JHUGen\ v7.0.9".format(self.powhegprocess)
+  def hasfilter(self): return "filter" in self.decaycard.lower()
+  @property
+  def tmptarball(self):
+    return os.path.join(here, "workdir", os.path.basename(self.powhegcard).replace(".input", "_"+self.decaymode),
+             self.powhegprocess+"_"+scramarch+"_"+cmsswversion+"_"+os.path.basename(self.powhegcard).replace(".input", "_"+self.decaymode+".tgz"))
+  @property
+  def queue(self):
+    if self.productionmode == "ggH": return "1nd"
+    if self.productionmode in ("ZH", "ttH"): return "1nw"
+    return "1nd"
+  @property
+  def makegridpackcommand(self):
+    args = {
+      "-p": "f",
+      "-i": self.powhegcard,
+      "-g": self.decaycard,
+      "-m": self.powhegprocess,
+      "-f": os.path.basename(self.powhegcard).replace(".input", "_"+self.decaymode),
+      "-q": self.queue,
+      "-n": "10",
+    }
+    return ["./run_pwg.py"] + sum(([k, v] for k, v in args.iteritems()), [])
+  @property
+  def makinggridpacksubmitsjob(self):
+    return "full_"+os.path.basename(self.powhegcard).replace(".input", "")
 
   @property
   @cache
   def cardsurl(self):
     powhegdir, powhegcard = os.path.split(self.powhegcard)
     powhegscript = os.path.join(powhegdir, "makecards.py")
-    commit = "118144fc626bc493af2dac01c57ff51ea56562c7"
+    commit = self.genproductionscommit
     powhegscript = os.path.join("https://raw.githubusercontent.com/cms-sw/genproductions/", commit, powhegscript.split("genproductions/")[-1])
-    JHUGencard = os.path.join("https://raw.githubusercontent.com/cms-sw/genproductions/", commit, self.JHUGencard.split("genproductions/")[-1])
+    JHUGencard = os.path.join("https://raw.githubusercontent.com/cms-sw/genproductions/", commit, self.decaycard.split("genproductions/")[-1])
 
     result = (       powhegscript + "\n"
             + "#    " + powhegcard + "\n"
@@ -734,6 +625,116 @@ class POWHEGJHUGenMassScanMCSample(MassScanMCSample, POWHEGJHUGenMCSample):
     return result
 
   @property
+  def generators(self):
+    return r"powheg\ {} JHUGen\ v7.0.9".format(self.powhegprocess)
+
+class POWHEGJHUGenMassScanMCSample(MassScanMCSample, POWHEGJHUGenMCSample):
+  @property
+  def powhegprocess(self):
+    if self.productionmode == "ggH": return "gg_H_quark-mass-effects"
+    if self.productionmode == "VBF": return "VBF_H"
+    if self.productionmode == "ZH": return "HZJ"
+    if self.productionmode in ("WplusH", "WminusH"): return "HWJ"
+    if self.productionmode == "ttH": return "ttH"
+    raise ValueError("Unknown productionmode "+self.productionmode)
+
+  @property
+  def powhegcard(self):
+    folder = os.path.join(genproductions, "bin", "Powheg", "production", "2017", "13TeV", self.powhegprocess+"_NNPDF31_13TeV")
+    makecards(folder)
+
+    cardbase = self.powhegprocess
+    if self.productionmode == "ZH": cardbase = "HZJ_HanythingJ"
+    if self.productionmode == "WplusH": cardbase = "HWplusJ_HanythingJ"
+    if self.productionmode == "WminusH": cardbase = "HWminusJ_HanythingJ"
+    if self.productionmode == "ttH": cardbase = "ttH_inclusive"
+    card = os.path.join(folder, cardbase+"_NNPDF31_13TeV_M{:d}.input".format(self.mass))
+
+    if not os.path.exists(card):
+      raise IOError(card+" does not exist")
+    return card
+
+  @property
+  def reweightdecay(self):
+    return self.mass >= 200
+
+  @property
+  def filter4L(self):
+    if self.decaymode != "4l": return False
+    if self.productionmode in ("ggH", "VBF", "WplusH", "WminusH"): return False
+    if self.productionmode in ("ZH", "ttH"): return True
+    raise ValueError("Unknown productionmode "+self.productionmode)
+
+  @property
+  def tarballversion(self):
+    v = 1
+
+    v+=1 #JHUGen version
+    if self.productionmode == "ggH" and self.decaymode == "2l2nu" and self.mass == 400: v+=1
+    if self.productionmode == "ggH" and self.decaymode == "4l" and self.mass in (300, 350, 400, 450, 500, 550, 600, 700, 750, 800, 900, 1000, 1500, 2000, 2500, 3000): v+=1  #core dumps in v2
+    if self.productionmode == "ggH" and self.decaymode == "2l2nu" and self.mass in (300, 400, 1000, 1500): v+=1   #core dumps in v1
+    if self.productionmode == "ggH" and self.decaymode == "2l2q" and self.mass == 750: v+=1   #core dumps in v1
+    if self.productionmode == "ZH" and self.decaymode == "4l" and self.mass == 145: v+=1   #core dumps in v2
+    if self.decaymode == "4l": v+=1  #v1 messed up the JHUGen decay card
+    if self.productionmode == "ggH" and self.decaymode == "2l2nu" and self.mass == 2500: v+=1  #v1 is corrupted
+    if self.productionmode == "ggH" and self.decaymode == "2l2q" and self.mass == 800: v+=1  #same
+
+    return v
+
+  @property
+  def cvmfstarball(self):
+    folder = os.path.join("/cvmfs/cms.cern.ch/phys_generator/gridpacks/2017/13TeV/powheg/V2", self.powhegprocess+"_NNPDF31_13TeV")
+    tarballname = os.path.basename(self.powhegcard).replace(".input", ".tgz")
+    if self.decaymode != "4l":
+      decaymode = self.decaymode
+      if "ZZ2l2any_withtaus.input" in self.decaycard: decaymode == "2l2X"
+      elif "ZZany_filter2lOSSF.input" in self.decaycard: decaymode = "_filter2l"
+      tarballname = tarballname.replace("NNPDF31", "ZZ"+self.decaymode+"_NNPDF31")
+    return os.path.join(folder, tarballname.replace(".tgz", ""), "v{}".format(self.tarballversion), tarballname)
+
+  @property
+  @cache
+  def olddatasetname(self):
+    p = self.productionmode
+    if p == "VBF": p = "VBFH"
+    with contextlib.closing(urllib.urlopen("https://raw.githubusercontent.com/CJLST/ZZAnalysis/miniAOD_80X/AnalysisStep/test/prod/samples_2016_MC.csv")) as f:
+      reader = csv.DictReader(f)
+      for row in reader:
+        if row["identifier"] == "{}{}".format(p, self.mass):
+          dataset = row["dataset"]
+          result = re.sub(r"^/([^/]*)/[^/]*/[^/]*$", r"\1", dataset)
+          assert result != dataset and "/" not in result, result
+          if self.decaymode == "4l":
+            return result
+    raise ValueError("Nothing for {}".format(self))
+
+  @property
+  def datasetname(self):
+    if self.decaymode == "2l2nu":
+      result = type(self)(self.productionmode, "4l", self.mass).datasetname.replace("4L", "2L2Nu")
+    elif self.decaymode == "2l2q":
+      result = type(self)(self.productionmode, "4l", self.mass).datasetname.replace("4L", "2L2Q")
+      if self.mass == 125:
+        if self.productionmode in ("VBF", "WplusH", "WminusH"): result = result.replace("2L2Q", "2L2X")
+        if self.productionmode == "ZH": result = "ZH_HToZZ_2LFilter_M125_13TeV_powheg2-minlo-HZJ_JHUGenV709_pythia8"
+        if self.productionmode == "ttH": result = "ttH_HToZZ_2LOSSFFilter_M125_13TeV_powheg2_JHUGenV709_pythia8"
+    elif self.productionmode in ("WplusH", "WminusH", "ZH") and self.mass > 230:
+      result = type(self)(self.productionmode, self.decaymode, 230).datasetname.replace("M230", "M{:d}".format(self.mass))
+    else:
+      result = self.olddatasetname.replace("JHUgenV698", "JHUGenV709").replace("JHUgenV6", "JHUGenV709")
+
+    pm = self.productionmode.replace("gg", "GluGlu")
+    dm = self.decaymode.upper().replace("NU", "Nu")
+    if self.decaymode == "2l2q" and self.mass == 125:
+      if self.productionmode in ("VBF", "WplusH", "WminusH"): dm = "2L2X"
+      if self.productionmode in ("ZH", "ttH"): dm = "Filter"
+    searchfor = [pm, dm, "M{:d}".format(self.mass), "JHUGenV709_"]
+    if any(_ not in result for _ in searchfor):
+      raise ValueError("Dataset name doesn't make sense:\n{}\n{}\n{}".format(result, searchfor, self))
+
+    return result
+
+  @property
   def nfinalparticles(self):
     if self.productionmode == "ggH": return 1
     if self.productionmode in ("VBF", "ZH", "WplusH", "WminusH", "ttH"): return 3
@@ -764,7 +765,7 @@ class POWHEGJHUGenMassScanMCSample(MassScanMCSample, POWHEGJHUGenMCSample):
   def fragmentname(self):
     return "Configuration/GenProduction/python/ThirteenTeV/Hadronizer/Hadronizer_TuneCP5_13TeV_powhegEmissionVeto_{:d}p_LHE_pythia8_cff.py".format(self.nfinalparticles)
   @property
-  def fragmenttag(self):
+  def genproductionscommit(self):
     return "118144fc626bc493af2dac01c57ff51ea56562c7"
 
 
