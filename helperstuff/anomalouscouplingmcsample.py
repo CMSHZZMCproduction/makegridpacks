84859961857846cbdbcf83d544926656633983db
Heshy Roskes <jroskes1@jhu.edu>
2018-01-17 21:40:44 +0100

    move some common code to anomalouscouplingmcsample

--- helperstuff/anomalouscouplingmcsample.py
+++ helperstuff/anomalouscouplingmcsample.py
@@ -43,6 +43,57 @@ class AnomalousCouplingMCSample(MCSampleBase):
     return card
 
   @property
+  def datasetname(self):
+    result = {
+      "VBF": "VBFHiggs",
+      "HJJ": "JJHiggs",
+      "ZH":  "ZHiggs",
+      "WH":  "WHiggs",
+      "ttH": "ttHiggs",
+    }[self.productionmode]
+    result += {
+      "SM": "0PM",
+      "a2": "0PH",
+      "a3": "0M",
+      "0M": "0M",  #for ttH
+      "L1": "0L1",
+      "L1Zg": "0L1Zg",
+      "a2mix": "0PHf05ph0",
+      "a3mix": "0Mf05ph0",
+      "0Mmix": "0Mf05ph0",  #for ttH
+      "L1mix": "0L1f05ph0",
+      "L1Zgmix": "0L1Zgf05ph0",
+    }[self.kind]
+
+    result += "ToZZ"
+    if "ZZ4l_withtaus" in self.decaycard:
+      result += "To4L"
+    elif "ZZ2l2any_withtaus_filter4l" in self.decaycard:
+      result += "_4LFilter"
+    else:
+      raise ValueError("Unknown decay card\n"+self.decaycard)
+
+    result += "_M125_13TeV_JHUGenV7011_pythia8"
+
+    pm = self.productionmode.replace("HJJ", "JJH").replace("H", "Higgs")
+    dm = self.decaymode.upper().replace("NU", "Nu")
+    searchfor = [pm, dm, "M{:d}".format(self.mass), "JHUGenV7011_"]
+    shouldntbethere = ["powheg"]
+    if any(_ not in result for _ in searchfor) or any(_.lower() in result.lower() for _ in shouldntbethere):
+      raise ValueError("Dataset name doesn't make sense:\n{}\n{}\nNOT {}\n{}".format(result, searchfor, shouldntbethere, self))
+
+    searchfor = result.replace("Zg", "").replace("JHUGenV7011", "JHUgenV6")
+    with contextlib.closing(urllib.urlopen("https://raw.githubusercontent.com/CJLST/ZZAnalysis/f7d5b5fecf322a8cffa435cfbe3f05fb1ae6aba2/AnalysisStep/test/prod/samples_2016_MC_anomalous.csv")) as f:
+      reader = csv.DictReader(f)
+      for row in reader:
+        if row["dataset"] and row["dataset"].split("/")[1] == searchfor:
+          break
+      else:
+        raise ValueError("Couldn't find dataset name {}".format(searchfor))
+
+    return result
+
+  @property
   def nevents(self):
     if self.decaymode == "4l":
       if self.productionmode in ("HJJ", "ttH"):
@@ -57,3 +108,24 @@ class AnomalousCouplingMCSample(MCSampleBase):
   @property
   def keepoutput(self):
     return False
+
+  @classmethod
+  def getmasses(cls, productionmode, decaymode):
+    if decaymode == "4l":
+      if productionmode == "ggH" or productionmode == "HJJ" or productionmode == "VBF" or productionmode == "ZH" or productionmode == "WH" or productionmode == "ttH":
+        return 125
+
+  @classmethod
+  def getkind(cls,productionmode,decaymode):
+    if productionmode == "ggH" :
+      return "L1","L1Zg","L1Zgmix","L1mix","SM","a2","a2mix","a3","a3mix" 
+    if productionmode == "HJJ" :
+      return "SM","a3","a3mix" 
+    if productionmode == "VBF" :
+      return "L1","L1Zg","L1Zgmix","L1mix","SM","a2","a2mix","a3","a3mix" 
+    if productionmode == "WH" :
+      return "L1","L1mix","SM","a2","a2mix","a3","a3mix" 
+    if productionmode == "ZH" :
+      return "L1","L1Zg","L1Zgmix","L1mix","SM","a2","a2mix","a3","a3mix" 
+    if productionmode == "ttH" :
+      return "0M","0Mmix","SM"
--- helperstuff/jhugenjhugenanomalouscouplings.py
+++ helperstuff/jhugenjhugenanomalouscouplings.py
@@ -43,57 +43,6 @@ class JHUGenJHUGenAnomCoupMCSample(AnomalousCouplingMCSample, JHUGenJHUGenMCSamp
     return os.path.join(folder, tarballname.replace(".tgz", ""), "v{}".format(self.tarballversion), tarballname)
 
   @property
-  def datasetname(self):
-    result = {
-      "VBF": "VBFHiggs",
-      "HJJ": "JJHiggs",
-      "ZH":  "ZHiggs",
-      "WH":  "WHiggs",
-      "ttH": "ttHiggs",
-    }[self.productionmode]
-    result += {
-      "SM": "0PM",
-      "a2": "0PH",
-      "a3": "0M",
-      "0M": "0M",  #for ttH
-      "L1": "0L1",
-      "L1Zg": "0L1Zg",
-      "a2mix": "0PHf05ph0",
-      "a3mix": "0Mf05ph0",
-      "0Mmix": "0Mf05ph0",  #for ttH
-      "L1mix": "0L1f05ph0",
-      "L1Zgmix": "0L1Zgf05ph0",
-    }[self.kind]
-
-    result += "ToZZ"
-    if "ZZ4l_withtaus" in self.decaycard:
-      result += "To4L"
-    elif "ZZ2l2any_withtaus_filter4l" in self.decaycard:
-      result += "_4LFilter"
-    else:
-      raise ValueError("Unknown decay card\n"+self.decaycard)
-
-    result += "_M125_13TeV_JHUGenV7011_pythia8"
-
-    pm = self.productionmode.replace("HJJ", "JJH").replace("H", "Higgs")
-    dm = self.decaymode.upper().replace("NU", "Nu")
-    searchfor = [pm, dm, "M{:d}".format(self.mass), "JHUGenV7011_"]
-    shouldntbethere = ["powheg"]
-    if any(_ not in result for _ in searchfor) or any(_.lower() in result.lower() for _ in shouldntbethere):
-      raise ValueError("Dataset name doesn't make sense:\n{}\n{}\nNOT {}\n{}".format(result, searchfor, shouldntbethere, self))
-
-    searchfor = result.replace("Zg", "").replace("JHUGenV7011", "JHUgenV6")
-    with contextlib.closing(urllib.urlopen("https://raw.githubusercontent.com/CJLST/ZZAnalysis/f7d5b5fecf322a8cffa435cfbe3f05fb1ae6aba2/AnalysisStep/test/prod/samples_2016_MC_anomalous.csv")) as f:
-      reader = csv.DictReader(f)
-      for row in reader:
-        if row["dataset"] and row["dataset"].split("/")[1] == searchfor:
-          break
-      else:
-        raise ValueError("Couldn't find dataset name {}".format(searchfor))
-
-    return result
-
-  @property
   def defaulttimeperevent(self):
     return 30
     assert False
@@ -113,26 +62,6 @@ class JHUGenJHUGenAnomCoupMCSample(AnomalousCouplingMCSample, JHUGenJHUGenMCSamp
     elif self.productionmode in ("VBF", "HJJ", "ZH", "WH"):
       return "Configuration/GenProduction/python/ThirteenTeV/Hadronizer/Hadronizer_TuneCP5_13TeV_pTmaxMatch_1_pTmaxFudge_half_LHE_pythia8_cff.py"
     raise ValueError("No fragment for {}".format(self))
-
-  @classmethod
-  def getmasses(cls, productionmode, decaymode):
-    if decaymode == "4l":
-      if productionmode == "HJJ" or productionmode == "VBF" or productionmode == "ZH" or productionmode == "WH" or productionmode == "ttH":
-        return 125
-
-  @classmethod
-  def getkind(cls,productionmode,decaymode):
-    if productionmode == "HJJ" :
-      return "SM","a3","a3mix" 
-    if productionmode == "VBF" :
-      return "L1","L1Zg","L1Zgmix","L1mix","SM","a2","a2mix","a3","a3mix" 
-    if productionmode == "WH" :
-      return "L1","L1mix","SM","a2","a2mix","a3","a3mix" 
-    if productionmode == "ZH" :
-      return "L1","L1Zg","L1Zgmix","L1mix","SM","a2","a2mix","a3","a3mix" 
-    if productionmode == "ttH" :
-      return "0M","0Mmix","SM" 
-
 
   @classmethod
   def allsamples(cls):
