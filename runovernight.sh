6a4b0ee68272e90496bbb0b9c624eaa2cb182174
Heshy Roskes <jroskes1@jhu.edu>
2018-01-30 16:23:55 +0100

    script to run overnight

---
+++ runovernight.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+(echo $STY) || (echo "run this on a screen"; exit 1)
+
+! (echo $CMSSW_BASE) || (echo "run this in a new session, without doing cmsenv"; exit 1)
+
+(eval $(scram ru -sh); echo $CMSSW_BASE) || (echo "run this inside a CMSSW release (but don't cmsenv)"; exit 1)
+
+for i in {1..48}; do (
+  source /afs/cern.ch/cms/PPD/PdmV/tools/McM/getCookie.sh
+  eval $(scram ru -sh)
+  ./makegridpacks.py
+) || true
+sleep 1h
+done
