d3089cf8a645a1650039ba6b55386522e82e5aef
Heshy Roskes <jroskes1@jhu.edu>
2018-04-30 17:38:17 +0200

    fix requesturl

--- requesturl.py
+++ requesturl.py
@@ -1,1 +1,40 @@
-helperstuff/requesturl.py
+#!/usr/bin/env python
+
+import argparse
+import pprint
+
+from helperstuff import allsamples
+from helperstuff.requesturl import requesturl_prepids
+
+def requesturl(filter=lambda sample: True, status=("defined",), onlymysamples=False):
+  prepids = [sample.prepid for sample in allsamples(
+    filter=lambda sample:
+      sample.prepid is not None
+      and filter(sample)
+      and sample.status in status,
+    onlymysamples=onlymysamples,
+  )]
+  if not prepids:
+    print "no prepids!"
+    return
+  return requesturl_prepids(prepids)
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--filter", "-f", type=eval, default=lambda sample: True)
+  group = parser.add_mutually_exclusive_group()
+  group.add_argument("--submitted", action="store_true", help="make a ticket using requests that are already approved or submitted")
+  group.add_argument("--unvalidated", action="store_true", help="make a ticket using requests that are not validated (status new)")
+  allstatuses = ("new", "validation", "defined", "approved", "submitted", "done")
+  group.add_argument("--status", action="append", help="make a ticket using requests that have the statuses listed here", choices=allstatuses)
+  group.add_argument("--all-statuses", action="store_true")
+  parser.add_argument("--everyones-samples", action="store_true", help="include samples that you are not the responsible person for")
+  args = parser.parse_args()
+
+  if args.submitted: status = ("approved", "submitted", "done")
+  elif args.unvalidated: status = ("new",)
+  elif args.status: status = args.status
+  elif args.all_Statuses: status = allstatuses
+  else: status = ("defined",)
+
+  print requesturl(filter=args.filter, status=status, onlymysamples=not args.everyones_samples)
